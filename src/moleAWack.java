f83dd72275897c602161ae767d8040e00ba22419
league <league@worthymini-5.attlocal.net>
2019-01-28 18:33:28 -0800

    whack a mole ip

--- src/moleAWack.java
+++ src/moleAWack.java
@@ -1,29 +1,56 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Date;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class moleAWack {
+public class moleAWack implements ActionListener {
 JFrame window = new JFrame( );
 JPanel panal = new JPanel();
-
-	public static void main(String[] args)    /* implements */    {
+JButton start = new JButton("start");
+	public static void main(String[] args)         {
 		moleAWack mole = new moleAWack();
-		mole.code();
-		mole.speak("anamel crackers");
-	}
-	void code() {
+		mole.code1();
+		//mole.code();
 		
-		window.setSize(300, 500);
-		for (int i = 1; i < 25; i+=1) {
+		//mole.speak("jackie,do  I  have a question?");
+	}
+	void code1(){
+		start.addActionListener(this);
+		panal.add(start);
+		window.add(panal);
+		window.setVisible(true);
+		window.setDefaultCloseOperation(window.EXIT_ON_CLOSE);
+		window.pack();
+	}
+	
+	void code() {
+		//////////////////////////////////////////////////////////////////////////////////////////////////////
+		Random bobTheRandom = new Random();
+		 int bobbyTheIntRandom = bobTheRandom.nextInt(24);
+		
+		
+		
+		for (int i = 0; i < 24; i+=1) {
 			JButton button = new JButton();
+			panal.add(button);
+			if (i == bobbyTheIntRandom) {
+				button.setText("mole");
+			}
+			button.addActionListener(this);
+		}
+		
+		
 		window.setVisible(true);
 		window.add(panal);
-		panal.add(button);
 		
-		}
+		window.setDefaultCloseOperation(window.EXIT_ON_CLOSE);
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		window.setSize(300, 500);							//set size
 	}
 	void speak(String words) {
     try {
@@ -32,12 +59,31 @@ JPanel panal = new JPanel();
         e.printStackTrace();
    }
 }
-	
 	private void endGame(Date timeAtStart, int molesWhacked) {
 	     Date timeAtEnd = new Date();
 	     JOptionPane.showMessageDialog(null, "Your whack rate is "
 	          + ((timeAtEnd.getTime() - timeAtStart.getTime()) / 1000.00 / molesWhacked)  + " moles per second.");
 	        
+	}
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if (start == e.getSource()) {
+			code();
+		}
+		
+		
+		JButton buttonPressed = (JButton) e.getSource();
+		if (buttonPressed.getText() ==  "mole") {
+			speak("oof");
+		}
+		else {
+			speak("not oof");
+
+		}
+		
 	}
 	
 	
